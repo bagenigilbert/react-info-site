cc73d052382fa412223cc008fb4f3c5d852a06b6
gilbert.bageni <135163732+gilbert2003-beep@users.noreply.github.com>
2023-07-19 22:14:55 +0300

    added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-export default function Navbar() {
+export default function Navbar(logo) {
     return (
-        <nav>
+        <nav>{logo}
             <img src="../images/react-icon-small.png" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
